072ecb69086bd69b0793b0be78f7a7998b3ca2ed
hnromante <hugonromante@outlook.com>
2017-12-14 16:04:30 -0300

    Agregado conf de conexion para luinux y windows

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -11,8 +11,12 @@ class AccesoDatos{
 
     protected function conexion(){
         //Inicializamos la variable conexion utilizando la clas mysqli propia de PHP
-        $this->con = new MySQLi("localhost","root","","id3524871_bd_clinicalourde") or die("Sin Conexion");
-        // $this->con = new MySQLi("localhost","id3524871_arcegod","miguelitopopwine","id3524871_clinicalourde") or die("Sin Conexion");
+        //WINDOWS
+        //$this->con = new MySQLi("localhost","root","","id3524871_clinicalourde") or die("Sin Conexion");
+        //LINUX
+        //$this->con = new MySQLi("localhost","root","","id3524871_bd_clinicalourde") or die("Sin Conexion");
+        //HOSTING
+        $this->con = new MySQLi("localhost","id3524871_arcegod","miguelitopopwine","id3524871_clinicalourde") or die("Sin Conexion");
 
         if($this->con->connect_errno){
             die('Error en la conexion! '.$this->con->connect_error);
